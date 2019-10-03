446d1630cc01d8f011e8be7a11bddc5c39817bb4
Niclas Binninge <alltjagvill@gmail.com>
2019-10-03 19:10:35 +0200

    Physics and moving platform

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+    public float speed;
+    Vector2 startVec2Pos;
+    Vector2 endVec2Pos;
+    private bool goingToEndPoint = true;
+    
+    void Start()
+    {
+        startVec2Pos = startPos.position;
+        endVec2Pos = endPos.position;
+        transform.position = startPos.position;
+       
+        
+    }
+    void Update()
+    {
+        //transform.position = Vector2.MoveTowards(transform.position, endVec2Pos, speed * Time.deltaTime);
+        if (goingToEndPoint == true )
+        {
+            transform.position = Vector2.MoveTowards(transform.position, endVec2Pos, speed * Time.deltaTime);
+            Debug.Log("Transform: " + transform.position);
+            Debug.Log("End: " + endPos.position);
+
+            if (transform.position == endPos.position)
+            {
+                goingToEndPoint = false;
+            }
+        }
+
+        if (goingToEndPoint == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startVec2Pos, speed * Time.deltaTime);
+            if (transform.position == startPos.position)
+            {
+                goingToEndPoint = true;
+            }
+        }
+    }
+
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,19 +11,28 @@ public class PlayerController : MonoBehaviour
     private bool facingRight = true;
     public float moveSpeed = 1.0f;
     public float jumpHeight = 2.0f;
-    public float jumpSpeedUp;
-    public float jumpSpeedSide;
-    public float jumpHeightSide;
+    public float jumpSpeed;
+    public float wallJumpHeight;
+    public float wallJumpSpeed;
+    float tmpSpeed;
+    public float movingPlatformDivider;
+    float ifInAirSpeed = 1;
+    private bool isGrounded;
+    public float playerRayCastDistance;
+
+    private bool touchingLeftWall;
+    private bool touchingRightWall;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
+
+
     private float moveInput;
-    public float groundCheckDistance = 0.01f;
-    public float wallCheckDistance = 0.01f;
-    public LayerMask groundLayer;
-    public LayerMask wallLayer;
     public bool jumpEnabled = true;
-    private bool runEnable = true;
+    public bool runEnable = true;
     public float leftCheckGroundPosDistance;
-    public float rightCheckGroundPosDistance;
-   
+    public float rightCheckGroundPosDistance;  
 
     enum Direction
     {
@@ -35,29 +44,64 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
+    
 
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        runEnable = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals ("MovingPlatform"))
+        {
+            tmpSpeed = moveSpeed;
+            MovingPlatformController movPlatCont = col.gameObject.GetComponent<MovingPlatformController>();
+            moveSpeed = tmpSpeed / movingPlatformDivider;
+            this.transform.parent = col.transform;
+            
+            
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("MovingPlatform"))
+        {
+            moveSpeed = tmpSpeed;
+            
+            this.transform.parent = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (isGrounded != true)
+        {
+            ifInAirSpeed = jumpSpeed;
+        }
+        else
+        {
+            ifInAirSpeed = 1;
+        }
     }
  
 
     private void FixedUpdate()
     {
-#if (UNITY_EDITOR)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         
-        //if (!runEnable)
-        //{
-        //    if (IsPlayerOnGround())
-        //        runEnable = true;
-        //}
+#if (UNITY_EDITOR)
+
+        if (!runEnable)
+        {
+            if (isGrounded)
+                runEnable = true;
+        }
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -65,12 +109,9 @@ public class PlayerController : MonoBehaviour
           
                 if (runEnable == true)
                 {
-                
-                rigidBody.velocity = new Vector2(moveInput * moveSpeed, rigidBody.velocity.y);
-                
-                //RunLeft();
+                rigidBody.velocity = new Vector2(moveInput * moveSpeed * ifInAirSpeed, rigidBody.velocity.y);
                 animator.SetBool(leftPressedBoolHash, true);
-                } 
+            } 
         }
 
         if (!Input.GetKey(KeyCode.A))
@@ -80,12 +121,12 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            spriteRenderer.flipX = true;
+            transform.localScale = new Vector2(-1, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            spriteRenderer.flipX = false;
+            transform.localScale = new Vector2(1, 1);
         }
 
         if (Input.GetKey(KeyCode.D))
@@ -96,9 +137,7 @@ public class PlayerController : MonoBehaviour
 
             {
                 
-                rigidBody.velocity = new Vector2(moveInput * moveSpeed, rigidBody.velocity.y);
-                
-                //RunRight();
+                rigidBody.velocity = new Vector2(moveInput * moveSpeed * ifInAirSpeed, rigidBody.velocity.y);           
                 animator.SetBool(rightPressedBoolHash, true);
             }        
         }
@@ -108,32 +147,108 @@ public class PlayerController : MonoBehaviour
                 animator.SetBool(rightPressedBoolHash, false);
             }
 
-        
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, playerRayCastDistance, whatIsWall);
+        Debug.Log(hit);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
-           // Jump();
+           JumpUp();
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && hit.collider != null)
+        {
+            runEnable = false;
+            rigidBody.velocity = new Vector2(wallJumpSpeed * hit.normal.x, wallJumpHeight);
+            transform.localScale = transform.localScale.x == 1 ? new Vector2(-1, 1) : Vector2.one;
+            // WallJump();
+        }
+
+
+        
+        //if (Input.GetKeyDown(KeyCode.Space) && touchingLeftWall == true && isGrounded != true)
+        //{
+        //    JumpLeftWall();
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Space) && touchingRightWall == true && isGrounded != true)
+        //{
+        //    JumpRightWall();
+        //}
+
 #endif
 
     }
 
-    
+    void WallJump()
+    {
+        
+    }
 
-   
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+    }
 
-    
+
+
+
+
+    private void JumpUp()
+    {
+        rigidBody.velocity = Vector2.up * jumpHeight;
+    }
+
+    private void JumpLeftWall()
+    {
+        runEnable = false;
+        //rigidBody.AddForce(Vector2.up * wallJumpHeight, ForceMode2D.Impulse);
+        //rigidBody.AddForce(Vector2.right * wallJumpSpeed, ForceMode2D.Impulse);
+        //rigidBody.velocity = Vector2.up * wallJumpHeight;
+        //rigidBody.velocity = Vector2.right * wallJumpSpeed;
+        rigidBody.velocity = new Vector2(wallJumpSpeed, wallJumpHeight);
+    }
+
+    private void JumpRightWall()
+    {
+        runEnable = false;
+        rigidBody.velocity = Vector2.up * wallJumpHeight;
+       // rigidBody.velocity = Vector2.left * wallJumpSpeed;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //private void Jump()
     //{
-        
-        
+
+
     //    if (IsPlayerOnGround())
     //    {
     //        runEnable = true;
-           
+
     //        rigidBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Force);
-           
+
     //    } 
     //    if(IsPlayerOnWall(Direction.left))
     //    {
@@ -143,7 +258,7 @@ public class PlayerController : MonoBehaviour
     //    {
     //        WallJump(Direction.right);
     //    }
-        
+
     //}
 
     //private void WallJump(Direction direction)
@@ -151,7 +266,7 @@ public class PlayerController : MonoBehaviour
     //{
     //    if (direction == Direction.left)
     //    {
-            
+
     //        runEnable = false;
     //        rigidBody.AddForce(Vector2.up * jumpHeightSide, ForceMode2D.Impulse);
     //        rigidBody.AddForce(Vector2.right * jumpSpeedSide, ForceMode2D.Impulse);
@@ -175,7 +290,7 @@ public class PlayerController : MonoBehaviour
     //}
 
 
-    
+
     //private bool IsPlayerOnGround()
     //{
     //    RaycastHit2D checkGroundMiddle = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
@@ -227,5 +342,5 @@ public class PlayerController : MonoBehaviour
     //        return false;
     //    }
     //}
-    
+
 }
